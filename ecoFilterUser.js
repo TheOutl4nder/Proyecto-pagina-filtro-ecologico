d8257da52d0dff8b7ad55e85e81f3807da2b0ab0
TheOutl4nder <37419487+TheOutl4nder@users.noreply.github.com>
2019-12-06 15:02:23 -0600

    Add files via upload

--- ecoFilterUser.js
+++ ecoFilterUser.js
@@ -1,6 +1,7 @@
 'use strict'
 //ISAAC GAUNA BLANCARTE 713903 (AGREGAR PRODUCTOS AL CARRITO DE COMPRAS)
 
+
 let urlCarrito = "http://localhost:3000/carrito"
 let urlProductos = "http://localhost:3000/products"
 let cart={products:[],size:0}
@@ -17,8 +18,7 @@ function addFilter(event){
         cartItem.id=0;
     else if (item===document.getElementById("product2"))
         cartItem.id=1;
-    //POR EL MOMENTO ES UN CARRITO UNIVERSAL YA QUE NO TENEMOS UN TOKEN PARA IDENTIFICAR AL USUARIO, LA INFO DEL CARRITO SE
-    //ALMACENA EN LOCAL-STORAGE EL ID DEL ITEM PARA USARLO EN EL CARRITO Y AHÍ HACE EL GET DEL SERVIDOR
+    //EL CARRITO SE ALMACENA EN LOCAL-STORAGE EL ID DEL ITEM PARA USARLO EN EL CARRITO Y AHÍ HACE EL GET DEL SERVIDOR
    localStorage.setItem("cartItem"+cartIndex,cartItem.id);
    localStorage.setItem("index",cartIndex);
    cartIndex++;
